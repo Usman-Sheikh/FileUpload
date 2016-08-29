313ec0d6e99899e006e3fd1af780aede04b06799
Usman Sheikh <sh.usmanikram@hotmail.com>
2016-08-29 20:35:51 +0500

    Initial

---
+++ FileUpload/Controllers/StudentsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FileUpload.Models;
+using System.IO;
+using System.Web.Hosting;
+
+namespace FileUpload.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private FileUploadContext db = new FileUploadContext();
+
+        // GET: Students
+        public ActionResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+
+        // GET: Students/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // GET: Students/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Student student, HttpPostedFileBase doc)
+        {
+
+            if (ModelState.IsValid && doc !=null)
+            {
+                var filename = Path.GetFileName(doc.FileName);
+                var extension = Path.GetExtension(filename).ToLower();
+                if (extension == ".jpg" || extension == ".png")
+                {
+                    var path = HostingEnvironment.MapPath(Path.Combine("~/Content/Images/", filename));
+                    doc.SaveAs(path);
+                    student.ImageUrl = "~/Content/Images/" + filename;
+                    db.Students.Add(student);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Document size must be less then 5MB");
+                    return View(student);
+                }
+               
+            }
+            ModelState.AddModelError("", "Photo is required");
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Student student,string ImageValue, HttpPostedFileBase doc)
+        {
+            if(ModelState.IsValid && doc != null)
+            {
+                var filename = Path.GetFileName(doc.FileName);
+                var extension = Path.GetExtension(filename).ToLower();
+                if (extension == ".jpg" || extension == ".png")
+                {
+                    var path = HostingEnvironment.MapPath(Path.Combine("~/Content/Images/", filename));
+                    doc.SaveAs(path);
+                    student.ImageUrl = "~/Content/Images/" + filename;
+                    db.Entry(student).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Document size must be less then 5MB");
+                    return View(student);
+                }
+
+            }
+            else if (ModelState.IsValid)
+            {
+
+                student.ImageUrl = ImageValue;
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            ModelState.AddModelError("", "Photo is required");
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Student student = db.Students.Find(id);
+            db.Students.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FileUpload/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FileUpload.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Display(Name ="Photo")]
+        public string ImageUrl { get; set; }
+    }
+}
